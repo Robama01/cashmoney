bb730102e58a7f036c53a31ef5c52c4b3f74c035
Robama01 <mahuba228@gmail.com>
2026-07-17 14:37:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -428,3 +428,7 @@ if __name__ == "__main__":
     init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
+    import os
+import psycopg2
+
+conn = psycopg2.connect(os.environ["DATABASE_URL"])
